a89e9844826b4b1349f47b675cc7b3eda6d30379
elenacarrasco <elenacfnb@gmail.com>
2022-03-26 12:57:42 +0100

    Create Results component

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FlexDiv } from "../../assets/styles/utils";
 import { List, ListItem } from "./styles";
 import file from "../../assets/images/file.svg";
 
-const Results = () => {
+const Results = ({ list }) => {
+	const [ filteredList, setFilteredList ] = useState({})
+
+	const groupItems = (items) => {
+		const uniqs = items.reduce((acc, val) => {
+			acc[val] = acc[val] === undefined ? 1 : acc[val] += 1;
+			return acc;
+		}, {});
+		setFilteredList(uniqs)
+	}
+
+	useEffect(() => {
+		list.length > 0 && groupItems(list)
+	}, [list]);
+
 	return (
 		<List>
-			<ListItem>
-				<FlexDiv>
-					<img src={file} width={50} height={40} alt="Buscar" />
-					<div>scss</div>
-				</FlexDiv>
-				<div>3</div>
-			</ListItem>
-			<ListItem>
-				<FlexDiv>
-					<img src={file} width={50} height={40} alt="Buscar" />
-					<div>scss</div>
-				</FlexDiv>
-				<div>3</div>
-			</ListItem>
-			<ListItem>
-				<FlexDiv>
-					<img src={file} width={50} height={40} alt="Buscar" />
-					<div>scss</div>
-				</FlexDiv>
-				<div>3</div>
-			</ListItem>
+			{ Object.keys(filteredList).length > 0 &&
+				Object.keys(filteredList).map((key, index) => (
+						<ListItem key={index}>
+							<FlexDiv>
+								<img src={file} width={50} height={40} alt="Buscar" />
+								<div>{key}</div>
+							</FlexDiv>
+							<div>{filteredList[key]}</div>
+						</ListItem>
+					)
+				)
+			 }
 		</List>
 	)
 }
