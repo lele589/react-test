9810aabd6e17a9cda85eec4ed520be185b8e77bb
elenacarrasco <elenacfnb@gmail.com>
2022-03-26 12:57:34 +0100

    Create Form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FlexDiv } from "../../assets/styles/utils";
 import { Input, InputContainer, Submit } from "./styles"
 import search from "../../assets/images/search.svg";
+import loader from "../../assets/images/loader.svg";
+import { useAxios } from "../../hooks/useAxios";
 
-const Form = () => {
+const Form = ({  setResults }) => {
+
+	// https://api.github.com/repos/argoproj/argo-site
+	// https://api.github.com/repos/lele589/expedicion-norte
+	const [inputValue, setInputValue] = useState('https://api.github.com/repos/lele589/expedicion-norte');
+	const { getRepoData, loading } = useAxios();
+
+	const handleOnChange = (event) => {
+		setInputValue(event.target.value)
+	}
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		getRepoData(inputValue).then(results => setResults(results))
+	}
+
 	return (
-		<InputContainer>
-			<FlexDiv flexDirection="column" justifyContent="flex-start">
-				<Input placeholder="https://api.github.com/repos/userName/repoName"/>
-			</FlexDiv>
-			<Submit>
-				<img src={search} width={21} height={21} alt="Buscar" />
-			</Submit>
-		</InputContainer>
+		<form onSubmit={handleSubmit}>
+			<InputContainer>
+				<FlexDiv flexDirection="column" justifyContent="flex-start">
+					<Input value={inputValue} onChange={handleOnChange} placeholder="https://api.github.com/repos/[USER]/[REPO]"/>
+				</FlexDiv>
+				<Submit>
+					{ loading ? (
+						<img src={loader} width={21} height={21} alt="Cargando" />
+					) : (
+						<img src={search} width={21} height={21} alt="Buscar" />
+					)}
+				</Submit>
+			</InputContainer>
+		</form>
 	)
 }
 
